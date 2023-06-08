e955687452632a19339dc93ffabc132b35bf5913
Kangkan Lahkar <kangkanlahkar1@gmail.com>
2023-06-08 11:49:26 +0530

    View inproved, border display inproved

--- src/components/ticTacToe.js
+++ src/components/ticTacToe.js
@@ -3,10 +3,7 @@ import Footer from "./footer";
 import Header from "./header";
 import RefreshIcon from "./refresh_icon";
 import '../App.css';
-function click(val){
-    console.log(val)
 
-}
 export default function TicTatcToe(){
     const [chance, setChance] = useState('X')
     const [board, setBoard] = useState([['','',''], ['','',''], ['','','']])
@@ -44,7 +41,7 @@ export default function TicTatcToe(){
         
         console.log(e.target.innerText)
         let temp = [...board];
-        if(e.target.innerText.length == 0){
+        if(e.target.innerText.length === 0){
             if(chance === 'X'){
                 temp[i][j] = 'X';
                 setBoard(temp);
@@ -81,19 +78,19 @@ export default function TicTatcToe(){
                     <table>
                         <tbody>
                             <tr>
-                                <td onClick={(e)=>setMove(0, 0, e)}>{board[0][0]}</td>
-                                <td onClick={(e)=>setMove(0, 1, e)}>{board[0][1]}</td>
-                                <td onClick={(e)=>setMove(0, 2, e)}>{board[0][2]}</td>
+                                <td onClick={(e)=>setMove(0, 0, e)} className="border-right border-buttom" style={{color:(board[0][0]==='X'?'yellow':'red')}}>{board[0][0]}</td>
+                                <td onClick={(e)=>setMove(0, 1, e)} className="border-right border-left border-buttom" style={{color:(board[0][1]==='X'?'yellow':'red')}}>{board[0][1]}</td>
+                                <td onClick={(e)=>setMove(0, 2, e)} className="border-left border-buttom" style={{color:(board[0][2]==='X'?'yellow':'red')}}>{board[0][2]}</td>
                             </tr>
                             <tr>
-                                <td onClick={(e)=>setMove(1,0, e)}>{board[1][0]}</td>
-                                <td onClick={(e)=>setMove(1,1, e)}>{board[1][1]}</td>
-                                <td onClick={(e)=>setMove(1,2, e)}>{board[1][2]}</td>
+                                <td onClick={(e)=>setMove(1,0, e)} className="border-right border-buttom border-top" style={{color:(board[1][0]==='X'?'yellow':'red')}}>{board[1][0]}</td>
+                                <td onClick={(e)=>setMove(1,1, e)} className="border-right border-left border-buttom border-top" style={{color:(board[1][1]==='X'?'yellow':'red')}}>{board[1][1]}</td>
+                                <td onClick={(e)=>setMove(1,2, e)} className="border-left border-buttom border-top" style={{color:(board[1][2]==='X'?'yellow':'red')}}>{board[1][2]}</td>
                             </tr>
                             <tr>
-                                <td onClick={(e)=>setMove(2,0, e)}>{board[2][0]}</td>
-                                <td onClick={(e)=>setMove(2,1, e)}>{board[2][1]}</td>
-                                <td onClick={(e)=>setMove(2,2, e)}>{board[2][2]}</td>
+                                <td onClick={(e)=>setMove(2,0, e)} className="border-right border-top" style={{color:(board[2][0]==='X'?'yellow':'red')}}>{board[2][0]}</td>
+                                <td onClick={(e)=>setMove(2,1, e)} className="border-right border-left border-top" style={{color:(board[2][1]==='X'?'yellow':'red')}}>{board[2][1]}</td>
+                                <td onClick={(e)=>setMove(2,2, e)} className="border-left border-top" style={{color:(board[2][2]==='X'?'yellow':'red')}}>{board[2][2]}</td>
                             </tr>
                         </tbody>
                     </table>
